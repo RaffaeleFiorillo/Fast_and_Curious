c6120a75c629c8cad7198a1a32e0591cb793f266
RaffaeleFiorillo <raffadeus8@gmail.com>
2020-12-10 18:42:23 -0100

    main code upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import link_functions as lf
+
+pygame.init()
+
+screen = pygame.display.set_mode((1080, 700))
+pygame.display.set_caption("Fast and Curious-Beta")
+
+efeitos = {"exit1": lf.exit_game, "main_menu": lf.main_menu, "continue": lf.game_menu, "new": lf.create_new_account,
+           "exit2": lf.exit_game_menu, "tutorial": lf.tutorial, "exit3": lf.game_menu, "story": lf.display_story,
+           "mai": lf.game_ai, "mpart": lf.game_parts, "save": lf.tutorial_s, "level_up": lf.tutorial_lu,
+           "enemy": lf.tutorial_e, "controls": lf.tutorial_c
+           }
+
+ef = "main_menu"
+while True:
+    ef = efeitos[ef](screen)
